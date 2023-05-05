0adba93d8c4b0a4f87382064b28c62606bc24833
Roshan Bhatia <roshan@timebyping.com>
2023-05-05 16:26:09 -0700

    add shasum to image name

--- pkg/shared/push.go
+++ pkg/shared/push.go
@@ -2,9 +2,10 @@ package shared
 
 import (
 	"context"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"os"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -35,51 +36,58 @@ func init() {
 }
 
 func pushImageToS3(ctx context.Context, image, bucket string) error {
-    cfg, err := config.LoadDefaultConfig(ctx)
-    if err != nil {
-        return fmt.Errorf("unable to load AWS SDK config: %v", err)
-    }
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to load AWS SDK config: %v", err)
+	}
 
-    if endpoint, ok := os.LookupEnv("AWS_S3_ENDPOINT"); ok {
-        // Use a custom endpoint resolver if AWS_S3_ENDPOINT is set
-        cfg.EndpointResolver = aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-            return aws.Endpoint{
-                URL:           endpoint,
-                HostnameImmutable: true,
-                SigningRegion: region,
-            }, nil
-        })
-    }
+	if endpoint, ok := os.LookupEnv("AWS_S3_ENDPOINT"); ok {
+		// Use a custom endpoint resolver if AWS_S3_ENDPOINT is set
+		cfg.EndpointResolver = aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				URL:           endpoint,
+				HostnameImmutable: true,
+				SigningRegion: region,
+			}, nil
+		})
+	}
 
-    s3Client := s3.NewFromConfig(cfg)
+	s3Client := s3.NewFromConfig(cfg)
 
-    // Create a Docker client
-    dockerClient, err := client.NewClientWithOpts(client.FromEnv)
-    if err != nil {
-        return fmt.Errorf("failed to create Docker client: %v", err)
-    }
-    defer dockerClient.Close()
+	// Create a Docker client
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("failed to create Docker client: %v", err)
+	}
+	defer dockerClient.Close()
 
-    // Save the image to a tarball
-    imageTarball := fmt.Sprintf("%s.tar", strings.ReplaceAll(image, ":", "_"))
-    imageReader, err := dockerClient.ImageSave(ctx, []string{image})
-    if err != nil {
-        return fmt.Errorf("failed to save Docker image: %v", err)
-    }
-    defer imageReader.Close()
+	// Save the image to a tarball
 
-    // Upload the image to S3
-    uploader := manager.NewUploader(s3Client)
-    result, err := uploader.Upload(ctx, &s3.PutObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(imageTarball),
-        Body:   imageReader,
-    })
-    if err != nil {
-        return fmt.Errorf("failed to upload image: %v", err)
-    }
+	imageReader, err := dockerClient.ImageSave(ctx, []string{image})
+	if err != nil {
+		return fmt.Errorf("failed to save Docker image: %v", err)
+	}
+	defer imageReader.Close()
 
-    fmt.Printf("Successfully uploaded %s to %s\n", image, result.Location)
+	// Calculate the SHA256 digest of the image
+	h := sha256.New()
+	if _, err := io.Copy(h, imageReader); err != nil {
+		return fmt.Errorf("failed to calculate image digest: %v", err)
+	}
+	digest := fmt.Sprintf("sha256:%x", h.Sum(nil))
 
-    return nil
+	// Upload the image to S3 with the digest as the filename
+	uploader := manager.NewUploader(s3Client)
+	result, err := uploader.Upload(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(fmt.Sprintf("%s:%s", image, digest)),
+		Body:   imageReader,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload image: %v", err)
+	}
+
+	fmt.Printf("Successfully uploaded %s to %s\n", image, result.Location)
+
+	return nil
 }
